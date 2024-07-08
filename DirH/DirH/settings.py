a377d1b8b3c6b25700c184c14604d44ba7ca0ce7
Juan <jcvalencias15@gmail.com>
2024-07-08 07:09:17 -0500

    nuevo intento123

--- DirH/DirH/settings.py
+++ DirH/DirH/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-from Back.DirH.variables import password
+from Back.variables import password
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
